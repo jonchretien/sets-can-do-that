5a53a0a361cc745c5be6633a34783aaf3d869474
Jon C. <jon.chretien@gmail.com>
2025-08-21 09:57:58 -0400

    fix: symmetric difference diagram

--- js/svg/diagramStrategies.js
+++ js/svg/diagramStrategies.js
@@ -15,7 +15,7 @@ export function getDiagramStrategy(methodName) {
 
 function createDifferenceDiagram() {
   return `
-    <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="difference diagram">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="difference diagram">
       <rect width="100%" height="100%" fill="#fff"/>
       <path d="M115 100c0-25.91 14.08-48.51 35-60.61A69.667 69.667 0 0 0 115 30c-38.66 0-70 31.34-70 70s31.34 70 70 70c12.75 0 24.7-3.43 35-9.39-20.92-12.1-35-34.7-35-60.61z" style="fill:#ea9930"/>
       <circle cx="115" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
@@ -25,7 +25,7 @@ function createDifferenceDiagram() {
 
 function createIntersectionDiagram() {
   return `
-    <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="intersection diagram">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="intersection diagram">
       <rect width="100%" height="100%" fill="#fff"/>
       <path d="M150 39.39c-20.92 12.1-35 34.71-35 60.61s14.08 48.51 35 60.61c20.92-12.1 35-34.71 35-60.61s-14.08-48.51-35-60.61z" style="fill:#ea9930"/>
       <circle cx="115" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
@@ -34,34 +34,28 @@ function createIntersectionDiagram() {
 }
 
 function createSymmetricDifferenceDiagram() {
-  return `
-    <svg viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="symmetric difference diagram" style="width: 100%; height: 100%; max-width: 100%; max-height: 100%;">
-      <rect width="100%" height="100%" fill="#fff"/>
-      <circle cx="160" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <circle cx="240" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <path d="M 80,150
-    A 80,80 0 1,1 200,219.2820323027551
-    A 80,80 0 0,0 200,80.7179676972449
-    A 80,80 0 1,1 80,150 Z" fill="#111" fill-opacity="0.22"/>
-      <path d="M 320,150
-    A 80,80 0 1,1 200,219.2820323027551
-    A 80,80 0 0,0 200,80.7179676972449
-    A 80,80 0 1,1 320,150 Z" fill="#111" fill-opacity="0.22"/>
-    </svg>`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="symmetric difference diagram">
+    <rect width="100%" height="100%" fill="#fff"/>
+    <path d="M115 100c0-25.91 14.08-48.51 35-60.61A69.667 69.667 0 0 0 115 30c-38.66 0-70 31.34-70 70s31.34 70 70 70c12.75 0 24.7-3.43 35-9.39-20.92-12.1-35-34.7-35-60.61zm70-70c-12.75 0-24.7 3.43-35 9.39 20.92 12.1 35 34.71 35 60.61s-14.08 48.51-35 60.61a69.667 69.667 0 0 0 35 9.39c38.66 0 70-31.34 70-70s-31.34-70-70-70z" style="fill:#ea9930"/>
+    <circle cx="115" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
+    <circle cx="185" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
+    <text transform="translate(99.37 50.4)">A</text>
+    <text transform="translate(190.014 50.4)">B</text>
+  </svg>`;
 }
 
 function createUnionDiagram() {
   return `
-    <svg viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="union diagram" style="width: 100%; height: 100%; max-width: 100%; max-height: 100%;">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="union diagram">
       <rect width="100%" height="100%" fill="#fff"/>
       <circle cx="160" cy="150" r="80" fill="#111" fill-opacity="0.22" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <circle cx="240" cy="150" r="80" fill="#111" fill-opacity="0.22" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
+      <circle cx="240" cy="150" r="80" fill="#111" fill-opacity="0.22" stroke="#111" stroke-width="2"/>
     </svg>`;
 }
 
 function createDisjointDiagram() {
   return `
-    <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="disjoint sets diagram">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="disjoint sets diagram">
       <rect width="100%" height="100%" fill="#fff"/>
       <circle cx="90" cy="100" r="50" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
       <circle cx="210" cy="100" r="50" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
@@ -69,14 +63,8 @@ function createDisjointDiagram() {
 }
 
 function createSubsetDiagram() {
-  // return `
-  //   <svg viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="subset diagram" style="width: 100%; height: 100%; max-width: 100%; max-height: 100%;">
-  //     <rect width="100%" height="100%" fill="#fff"/>
-  //     <circle cx="200" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-  //     <circle cx="200" cy="150" r="50" fill="#111" fill-opacity="0.22" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-  //   </svg>`;
   return `
-  <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="subset diagram">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="subset diagram">
     <rect width="100%" height="100%" fill="#fff"/>
     <circle cx="150" cy="100" r="75" fill="#fff" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
     <circle cx="150" cy="120" r="50" fill="#fff" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
@@ -87,7 +75,7 @@ function createSubsetDiagram() {
 
 function createSupersetDiagram() {
   return `
-  <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="superset diagram">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200" role="img" aria-label="superset diagram">
     <rect width="100%" height="100%" fill="#fff"/>
     <circle cx="150" cy="100" r="75" fill="#fff" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
     <circle cx="150" cy="120" r="50" fill="#fff" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
