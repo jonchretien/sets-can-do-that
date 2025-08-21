29c96705d0f339a10a90a339b16be6c5ba20c6e5
Jon C. <jon.chretien@gmail.com>
2025-08-21 09:34:15 -0400

    fix: difference svg styling

--- js/svg/diagramStrategies.js
+++ js/svg/diagramStrategies.js
@@ -15,14 +15,11 @@ export function getDiagramStrategy(methodName) {
 
 function createDifferenceDiagram() {
   return `
-    <svg viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="difference diagram" style="width: 100%; height: 100%; max-width: 100%; max-height: 100%;">
+    <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="difference diagram">
       <rect width="100%" height="100%" fill="#fff"/>
-      <circle cx="160" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <circle cx="240" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <path d="M 80,150
-    A 80,80 0 1,1 200,219.2820323027551
-    A 80,80 0 0,0 200,80.7179676972449
-    A 80,80 0 1,1 80,150 Z" fill="#111" fill-opacity="0.22"/>
+      <path d="M115 100c0-25.91 14.08-48.51 35-60.61A69.667 69.667 0 0 0 115 30c-38.66 0-70 31.34-70 70s31.34 70 70 70c12.75 0 24.7-3.43 35-9.39-20.92-12.1-35-34.7-35-60.61z" style="fill:#ea9930"/>
+      <circle cx="115" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
+      <circle cx="185" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
     </svg>`;
 }
 
