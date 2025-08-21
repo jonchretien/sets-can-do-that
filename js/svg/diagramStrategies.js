1dac0243a1a143e67aa59a720aa7b9f7044cf206
Jon C. <jon.chretien@gmail.com>
2025-08-21 09:19:49 -0400

    fix: intersection diagram

--- js/svg/diagramStrategies.js
+++ js/svg/diagramStrategies.js
@@ -28,11 +28,11 @@ function createDifferenceDiagram() {
 
 function createIntersectionDiagram() {
   return `
-    <svg viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="intersection diagram" style="width: 100%; height: 100%; max-width: 100%; max-height: 100%;">
+    <svg viewBox="0 0 300 200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="intersection diagram">
       <rect width="100%" height="100%" fill="#fff"/>
-      <circle cx="160" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <circle cx="240" cy="150" r="80" fill="#000" fill-opacity="0.06" stroke="#111" stroke-opacity="0.4" stroke-width="2"/>
-      <ellipse cx="200" cy="150" rx="69.2820323027551" ry="64" fill="#111" fill-opacity="0.22"/>
+      <path d="M150 39.39c-20.92 12.1-35 34.71-35 60.61s14.08 48.51 35 60.61c20.92-12.1 35-34.71 35-60.61s-14.08-48.51-35-60.61z" style="fill:#ea9930"/>
+      <circle cx="115" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
+      <circle cx="185" cy="100" r="70" fill-opacity="0.06" stroke="#999" stroke-width="2"/>
     </svg>`;
 }
 
